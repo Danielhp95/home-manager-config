7a2d85477c5a416bd5a25741b6f6f39fcf8f40e9
Daniel Hernandez <daniel.hernandez2@sony.com>
2023-08-16 17:14:44 -0600

    Updated nixpkgs to 22.11, removed imagine-nvim and added nix.registry to have nixos (latest) and stable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   description = "Home Manager configurations";
 
   inputs = {
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    stable.url = "github:NixOS/nixpkgs/release-22.11";
     homeManager.url = "github:nix-community/home-manager";
     homeManager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixgl.url = "github:guibou/nixGL";
+    nixgl.inputs.nixpkgs.follows = "nixpkgs";
 
     nix2vim.url = "github:gytis-ivaskevicius/nix2vim?rev=f3b56da72278cd720fe7fb4b6d001047b7179669";
     nix2vim.inputs.nixpkgs.follows = "nixpkgs";
@@ -13,41 +18,37 @@
     nvfetcher.url = "github:berberman/nvfetcher";
     nvfetcher.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: remove this when finished
-    imagine-nvim = {
-      url = "path:/home/daniel/Projects/imagine.nvim";
-      flake = false;
-    };
+    haumea.url = "github:nix-community/haumea";
+    haumea.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = inputs @ { self, nixpkgs, nix2vim, nvfetcher, homeManager, imagine-nvim}: {
-      homeConfigurations = {
-        "daniel@fell-omen" = homeManager.lib.homeManagerConfiguration {
-          # pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          pkgs = import nixpkgs {
-            overlays = [
-                nix2vim.overlay
-                (import ./pkgs)
-                (import ./overlays.nix)
-                # imagine-nvim.overlay
-                # The overlay from imagine-nvim should replicate this behaviour
-                (final: prev: {
-                  vimPlugins = prev.vimPlugins // {
-                    # TODO: remove this!
-                      imagine-nvim = prev.vimUtils.buildVimPlugin {
-                        name = "imagine-nvim";
-                        src = inputs.imagine-nvim;
-                      };
-                    };
-                })
-              ];
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          modules = [
-            ./home.nix
-          ];
-        };
+  outputs = inputs @ { self, nixpkgs, nixgl, nix2vim, stable, nvfetcher, homeManager, haumea}:
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [
+        nixgl.overlay
+        nix2vim.overlay
+        (import ./pkgs)
+        (import ./overlays.nix)
+      ];
+    };
+  in {
+    homeConfigurations = {
+      "daniel@fell-omen" = homeManager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./home.nix
+          {  # Inlining an attribute set
+            nix.registry = {
+              nixos.flake = nixpkgs;
+              stable.flake = stable;
+            };
+          }
+        ];
       };
+    };
     };
 }
