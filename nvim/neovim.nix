3484322aa315d591921e5d98ce69d207f5eca3e9
Daniel Hernandez <daniel.hernandez2@sony.com>
2023-08-16 17:22:48 -0600

    Renaming of inputs to file?

--- nvim/neovim.nix
+++ nvim/neovim.nix
@@ -1,4 +1,4 @@
-args@{ lib, pkgs, ... }:
+args@{ pkgs, inputs, ... }:
 {
   home.packages = with pkgs; [
     gcc gnumake  # Required for telescope-fzf-native. Ideally we will find away of defining these elsewhere
