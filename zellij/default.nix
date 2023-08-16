e7f5c3060efb295e2e5bcdf80d4cb06693f7742b
Daniel Hernandez <daniel.hernandez2@sony.com>
2023-08-16 17:09:58 -0600

    Added Zellij

--- zellij/default.nix
+++ zellij/default.nix
@@ -2,7 +2,11 @@
 {
   programs.zellij = {
     enable = true;
-    package = pkgs.zellij.overrideAttrs ( oldAttrs: { version = "0.35.1"; } );
-    # settings = zellijConf;
+    package = pkgs.zellij.overrideAttrs ( oldAttrs: { version = "0.36"; } );
+    settings = {
+      ui.pane_frames.rounded_corners = true;
+      copy_on_select = false;
+      scrollback_editor = "nvim --clean";
+    };
   };
 }
