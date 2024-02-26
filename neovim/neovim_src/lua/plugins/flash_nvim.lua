e9a0f79376517db1248cf2907b0c020c48265efd
Daniel Hernandez <daniel.hernandez2@sony.com>
2024-02-25 17:09:20 -0700

    added flash.nvim

--- neovim/neovim_src/lua/plugins/flash_nvim.lua
+++ neovim/neovim_src/lua/plugins/flash_nvim.lua
@@ -5,14 +5,6 @@ return {
 		opts = {},
 		keys = {
 			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
 				"S",
 				mode = { "n", "x", "o" },
 				function()
