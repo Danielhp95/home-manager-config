79b9f62cfbf8572249475e0b562c790664f9b9cc
Daniel Hernandez <daniel.hernandez2@sony.com>
2024-04-09 21:50:23 -0600

    removed revoked secrets

--- sony_ai/default.nix
+++ sony_ai/default.nix
@@ -18,10 +18,11 @@ in
   ];
 
   # Ensure that credentials are stored in `.netrc`
-  home.file.".config/pip/pip.conf".source = ./pip.conf;
+  # home.file.".config/pip/pip.conf".source = ./pip.conf;
+  # home.file.".config/pypoetry/auth.toml".source = ./poetry_auth.toml;
+
   home.file.".docker/config.json".source = ./docker_config.json;
   home.file.".config/sai_docker/config.yaml".source = ./sai_docker_config.yaml;
-  home.file.".config/pypoetry/auth.toml".source = ./poetry_auth.toml;
 
   # environment.etc."fixuid/config.yml".source = ./fixuid_config.yml
   home.activation.cloneRepo = lib.hm.dag.entryAfter ["writeBoundary"] ''
