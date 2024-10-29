c38a20d50a72e42c1e8700e953f0cdd9406f44c3
Daniel Hernandez <daniel.hernandez2@sony.com>
2024-10-29 17:10:28 -0400

    removed unused hyprls

--- neovim/neovim_src/lua/aucmds.lua
+++ neovim/neovim_src/lua/aucmds.lua
@@ -8,16 +8,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
-
--- Starts hyprlang server 'hyprls'
--- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
--- 	pattern = { "*.hl", "hypr*.conf" },
--- 	callback = function(event)
--- 		print(string.format("starting hyprls for %s", vim.inspect(event)))
--- 		vim.lsp.start({
--- 			name = "hyprlang",
--- 			cmd = { "hyprls" },
--- 			root_dir = vim.fn.getcwd(),
--- 		})
--- 	end,
--- })
