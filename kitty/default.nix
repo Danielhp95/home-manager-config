44fdec33b4a5159681dd7792cc7c716addd59f3c
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-03-28 10:56:40 -0400

    using unstable kitty

--- kitty/default.nix
+++ kitty/default.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ inputs, ... }:
 
 {
   programs.kitty = {
     enable = true;
+    package = inputs.unstable.legacyPackages.x86_64-linux.kitty;
     extraConfig = builtins.readFile ./kitty.conf;
     themeFile = "Dracula";
   };
