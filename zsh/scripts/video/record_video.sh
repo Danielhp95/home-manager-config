fc9aec654612b404d496d004199203d6299f39c1
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-03-28 10:55:03 -0400

    Added todo on video recording script

--- zsh/scripts/video/record_video.sh
+++ zsh/scripts/video/record_video.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# TODO: Make this into a nix file using writeSriptBin
+# TODO: make these go to a folder
+# TODO: figure out how to get audio WITHOUT READING FROM MICROPHONES
 
 getdate() {
     date '+%Y%m%d_%H-%M-%S'
@@ -11,9 +14,9 @@ if pgrep wf-recorder > /dev/null; then
 else
     notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'record-script.sh'
     if [[ "$1" == "--sound" ]]; then
-        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor & disown
+        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" --audio & disown
     elif [[ "$1" == "--fullscreen-sound" ]]; then
-        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor & disown
+        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t & disown
     elif [[ "$1" == "--fullscreen" ]]; then
         wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t & disown
     else 
