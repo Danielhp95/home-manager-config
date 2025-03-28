5e90c3de13003114f99f72f3b1a3995ae4381ed2
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-03-28 10:59:27 -0400

    refactored and removed basedpyright

--- flake.nix
+++ flake.nix
@@ -6,8 +6,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    basedpyright_stable.url =
-      "github:nixos/nixpkgs?rev=2893f56de08021cffd9b6b6dfc70fd9ccd51eb60";
     hyprland = {
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
@@ -40,39 +38,49 @@
       inputs.hyprland.follows = "hyprland";
     };
 
-    hyprspace = {
-      url = "github:KZDKM/Hyprspace";
-      inputs.hyprland.follows = "hyprland";
-    };
-
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     danvim.url = "path:/home/daniel/nix_config/danvim";
-    danvim.inputs.nixpkgs.follows = "unstable";
+    # Try soon if we can go back to following unstable
+    # danvim.inputs.nixpkgs.follows = "unstable";
 
     haumea.url = "github:nix-community/haumea";
     haumea.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, stable, home-manager, ... }@inputs:
+  outputs =
+    {
+      nixpkgs,
+      stable,
+      home-manager,
+      ...
+    }@inputs:
     let
       system = "x86_64-linux";
       stableWithUnfree = import stable {
-        config = { allowUnfree = true; };
+        config = {
+          allowUnfree = true;
+        };
         inherit system;
       };
       unstableWithUnfree = import inputs.unstable {
-        config = { allowUnfree = true; };
+        config = {
+          allowUnfree = true;
+        };
         inherit system;
       };
-    in {
+    in
+    {
       # NixOS configuration entrypoint
       # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
         fell-omen = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+          specialArgs = {
+            inherit inputs;
+            unstableWithUnfree = unstableWithUnfree;
+          }; # Pass flake inputs to our config
           # > Our main nixos configuration file <
           modules = [
             # ./fcitx5
@@ -89,10 +97,16 @@
               home-manager = {
                 useGlobalPkgs = true;
                 useUserPackages = true;
-                sharedModules =
-                  [ ./swww/swww.nix ./hyprland/pyprland.nix ];
+                sharedModules = [
+                  ./swww/swww.nix
+                  ./hyprland/pyprland.nix
+                ];
                 extraSpecialArgs = { inherit inputs stableWithUnfree unstableWithUnfree; };
-                users.daniel = { config, ... }: { imports = [ ./home.nix ]; };
+                users.daniel =
+                  { config, ... }:
+                  {
+                    imports = [ ./home.nix ];
+                  };
               };
             }
             # overlays
@@ -101,7 +115,13 @@
                 (final: prev: {
                   # load in inputs and provide as `channels` attribute in `pkgs.channels`
                   channels = pipe inputs [
-                    (filterAttrs (name: _: elem name [ "nixpkgs" "unstable" ]))
+                    (filterAttrs (
+                      name: _:
+                      elem name [
+                        "nixpkgs"
+                        "unstable"
+                      ]
+                    ))
                     (mapAttrs (_: c: c.legacyPackages.${prev.system}))
                   ];
                   # override specific packages from unstable
@@ -110,14 +130,19 @@
                   # Hyprland specifics
                   inherit (inputs.hy3.packages.${prev.system}) hy3;
                   inherit (inputs.hyprland.packages.${prev.system})
-                    hyprland xdg-desktop-portal-hyprland hyprland-share-picker;
+                    hyprland
+                    xdg-desktop-portal-hyprland
+                    hyprland-share-picker
+                    ;
                 })
                 (import ./overlays.nix)
               ];
               nixpkgs.config.allowUnfree = true;
               nixpkgs.config.nvidia.acceptLicense = true;
-              nixpkgs.config.permittedInsecurePackages =
-                [ "electron-25.9.0" "zoom" ];
+              nixpkgs.config.permittedInsecurePackages = [
+                "electron-25.9.0"
+                "zoom"
+              ];
               # allows running packages from `nix run unstable#ansel`
               # `nix run nixos#lsd` is very fast as it uses local cache
               nix.registry = {
@@ -133,8 +158,7 @@
       # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
         "dani@fell-omen" = home-manager.lib.homeManagerConfiguration {
-          pkgs =
-            nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
           extraSpecialArgs = {
             inherit inputs;
           }; # Pass flake inputs to our config
