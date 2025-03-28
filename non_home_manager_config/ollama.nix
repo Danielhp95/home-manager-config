f2e8edecba8fcbfdfbd387cae5b2808d4ac7c95a
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-03-28 10:55:31 -0400

    added unstablae packages for ollama and open-webui

--- non_home_manager_config/ollama.nix
+++ non_home_manager_config/ollama.nix
@@ -1,7 +1,8 @@
 ## Ollama
-{ config, ... }:
+{ config, inputs, unstableWithUnfree, ... }:
 {
   services.ollama = {
+    package = unstableWithUnfree.ollama;
     enable = true;
     # package = pkgs.ollama-rocm;
     host = "0.0.0.0";
@@ -13,16 +14,13 @@
   };
 
   services.open-webui = {
+    # package = inputs.unstable.legacyPackages.x86_64-linux.open-webui;
     enable = true;
     stateDir = "/var/lib/open-webui";
     host = "0.0.0.0";
     # options: https://docs.openwebui.com/getting-started/advanced-topics/env-configuration
     environment = {
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
-      OLLAMA_API_BASE_URL = "http://127.0.0.1:${toString config.services.ollama.port}";
-      WEBUI_AUTH = "False";
+      OLLAMA_BASE_URL = "http://127.0.0.1:${toString config.services.ollama.port}";
     };
   };
 }
