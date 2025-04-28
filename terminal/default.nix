87f62dcb8a53490bf248a3d70550fce9264bdd1e
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-04-28 09:45:03 -0400

    Added new programs to terminal.nix

--- terminal/default.nix
+++ terminal/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ...}:
+{ pkgs, inputs, ... }:
 {
   home.packages = with pkgs; [
     gh
@@ -6,9 +6,38 @@
     powerline-fonts
   ];
   home.sessionVariables.TERM = "wezterm";
-  programs.wezterm = {
-    enable = true;
-    extraConfig = builtins.readFile ./wezterm.lua;
-    enableZshIntegration = true;
+  programs = {
+    # Terminal, ofc
+    wezterm = {
+      enable = true;
+      extraConfig = builtins.readFile ./wezterm.lua;
+      enableZshIntegration = true;
+    };
+    # Telescope inspired fzf for shell
+    television = {
+      enable = true;
+      settings = {
+        ui = {
+          show_preview_panel = false;
+          input_bar_position = "bottom";
+        };
+      };
+    };
+    # Really nice shell history
+    atuin = {
+      enable = true;
+      flags = [ "--disable-up-arrow" ];
+      settings = {
+        enter_accept = true; # Enter to execute, tab to select
+        show_help = false;
+        show_tabs = false;
+        invert = true;
+      };
+    };
+    # `ls` replacement
+    eza.enable = true;
+    # The one, the fuzzy searcher
+    fzf.enable = true;
   };
+
 }
