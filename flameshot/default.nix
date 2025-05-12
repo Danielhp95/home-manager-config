79ac2cae51aab3fbb14ab909d2d03d346754f92e
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-05-12 17:06:11 -0400

    updated flameshot (that I don't use!)

--- flameshot/default.nix
+++ flameshot/default.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 {
   # Hopefully this will work at some point
-  services.flameshot.enable = false;
+  services.flameshot = {
+    enable = true;
+    package = (pkgs.flameshot.override { enableWlrSupport = true;});
+    settings.General = {
+      showStartupLaunchMessage = false;
+      saveLastRegion = false;
+    };
+  };
 }
