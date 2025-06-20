3addd45ba420b348453f68a8a41bc389722a0f3a
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-06-20 08:12:01 -0400

    chore(theming): minor refactor

--- hyprland/theming.nix
+++ hyprland/theming.nix
@@ -10,6 +10,7 @@ in
       AdwStyleManager = "color-scheme=ADW_COLOR_SCHEME_PREFER_DARK";
     };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
     theme = {
       package = pkgs.whitesur-gtk-theme.override { themeVariants = [ "orange" ]; };
       name = "WhiteSur-Dark-orange";
@@ -25,17 +26,22 @@ in
       size = 11;
     };
   };
-  qt = {
-    enable = true;
-    theme = {
-      package = pkgs.whitesur-qt-theme.override { themeVariants = [ "orange" ]; };
-      name = "WhiteSur-Dark-orange";
-    };
-    iconTheme = {
-      package = pkgs.morewaita-icon-theme;
-      name = "MoreWaita";
-    };
+
+  # Cursor. This might not be necessary with hyprland 0.41
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = cursor-theme-name;
+    size = 20;
   };
+
+  home.sessionVariables = {
+    # TODO: this was introduced in November 2024, can we remove this at some point?
+    # To surpress error: GSK-message Error 71 (Protocol error) dispatching to Wayland display.
+    QT_QPA_PLATFORMTHEME = "gtk4";
+    GTK_THEME = "WhiteSur-Dark-orange"; # For nautilus. Not working
+  };
+
   dconf.settings = {
     "org/gnome/desktop/interface".cursor-theme = cursor-theme-name;
     "org/gnome/desktop/interface".color-scheme = "prefer-dark";
