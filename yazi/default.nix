ccd89ba74b5e6d1c4729499a1db9c83510790ec8
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-06-20 08:19:59 -0400

    feat(yazi):updated plugins

--- yazi/default.nix
+++ yazi/default.nix
@@ -7,7 +7,7 @@ let
   officialPlugins = pkgs.fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "4b027c79371af963d4ae3a8b69e42177aa3fa6ee";
+    rev = "c648526665f6fb466150e9ee41aa0431b87cb041";
     sha256 = "sha256-auGNSn6tX72go7kYaH16hxRng+iZWw99dKTTUN91Cow=";
   };
 in
@@ -22,12 +22,14 @@ in
       toggle-pane = "${officialPlugins}/toggle-pane.yazi";
       mount = "${officialPlugins}/mount.yazi";
       vcs-files = "${officialPlugins}/vcs-files.yazi";
+      zoom = "${officialPlugins}/zoom.yazi";
       fg = pkgs.fetchFromGitHub {
         owner = "DreamMaoMao";
         repo = "fg.yazi";
-        rev = "652d02a1413d2440d264667608102eb158ed0e68";
+        rev = "46a5c16f62f415f691319f984b9548249b0edc96";
         hash = "sha256-/GApLVDpGcH2drwSNluEvoQdnjgE8AsPHdci/9eg7Lg=";
       };
+      # TODO: add https://github.com/KKV9/compress.yazi
     };
     keymap.manager.prepend_keymap = lib.flatten [
       {
@@ -88,10 +90,33 @@ in
         ];
         run = "plugin fg fd";
       }
+      {
+        on = "+";
+        run = "plugin zoom 1";
+        desc = "Zoom in hovered file";
+      }
+      {
+        on = "-";
+        run = "plugin zoom -1";
+        desc = "Zoom out hovered file";
+      }
     ];
+    settings = {
+      tasks = {
+        # To render images past a given size
+        image_bound = [
+          10000
+          10000
+        ];
+      };
+      input = {
+        cursor_blink = true;
+      };
+    };
   };
   home.packages = with pkgs; [
     exiftool # Tool to read, write and edit EXIF meta information
     simple-mtpfs
+    imagemagick  # For resizing preview images
   ];
 }
