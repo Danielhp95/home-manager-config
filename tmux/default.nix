d93993b24296447e4b99a92c57433728aa951204
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-07-26 22:10:46 +0200

    feat(tmux): re-added tmux!

--- tmux/default.nix
+++ tmux/default.nix
@@ -1,23 +1,51 @@
 { pkgs, ... }:
 
-let
-  tmuxConf = builtins.readFile ./tmux.conf;
-in
+# TODO:
+# - images on yazi don't show up (they do in wezterm!)
 {
+  home.packages = [ pkgs.gitmux ];
   programs.tmux = {
-    enable = false;
-    shortcut = "a";
-    clock24 = true;
-    # Start windows and panes index at 1, not 0
-    baseIndex = 1;
-    aggressiveResize = true;
-    escapeTime = 0;
-    shell = "${pkgs.zsh}/bin/zsh";
-    terminal = "default-terminal";
-    extraConfig = tmuxConf;
-    plugins = with pkgs; [
-      { plugin = tmuxPlugins.resurrect; }
-      { plugin = tmuxPlugins.continuum; }
+    enable = true;
+    keyMode = "vi";
+    terminal = "kitty"; # I don't like this hardcoding!
+    extraConfig = builtins.readFile ./tmux.conf;
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-processes 'vim nvim ssh npm ~ipython'
+          set -g @resurrect-capture-pane-contents 'on' # Restore pane contents
+          set -g @resurrect-auto-restore 'on'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-boot 'on'
+          set -g @continuum-restore 'on' # Continuum auto restore
+          set -g @continuum-save-interval '5' # Save every 5 mins
+        '';
+      }
+      {
+        plugin = tmux-floax;
+        extraConfig = ''
+          set -g @floax-bind 'F'
+          set -g @floax-width '90%'
+          set -g @floax-height '90%'
+        '';
+      }
+      {
+        # https://github.com/alexwforsythe/tmux-which-key
+        plugin = tmux-which-key;
+        extraConfig = ''
+          set -g @tmux-which-key-xdg-enable 1;
+          set -g @tmux-which-key-disable-autobuild 1
+        '';
+      }
+      # Too look at some point, i3 style automatic layouts in tmux
+      # https://github.com/jabirali/tmux-tilish
     ];
   };
 }
