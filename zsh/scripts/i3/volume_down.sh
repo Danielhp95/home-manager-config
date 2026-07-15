51804c9468a9f026068ab2276c2513b20e352b82
Daniel Hernandez <daniel.hernandez2@sony.com>
2026-07-15 19:23:50 +0200

    remove(wm): legacy i3, sway, niri, swww and fusuma configs
    Superseded by the hyprland + noctalia + awww stack. Also drops the old
    i3 helper scripts (screenlayouts, volume, wallpaper) and the retired
    fell_omen hardware profile.

--- zsh/scripts/i3/volume_down.sh
+++
@@ -1,2 +0,0 @@
-VOLUME='-5%'; for SINK in 0
-4 ; do pactl set-sink-volume 4 -5% ; done
--- zsh/scripts/i3/wallchange-hangmoon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-  feh --randomize --bg-scale ~/nix_config/wallpapers/*
-  sleep 150
-done
